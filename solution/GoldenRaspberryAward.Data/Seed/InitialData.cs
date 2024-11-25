85f360d52ccb9da5d4ca494567ff88888a0889fe
Anderson Tomazeto <packetspy@gmail.com>
2024-11-25 15:21:33 -0300

    Refactor: load initial data

--- solution/GoldenRaspberryAward.Data/Seed/InitialData.cs
+++ solution/GoldenRaspberryAward.Data/Seed/InitialData.cs
@@ -15,10 +15,11 @@ public static class InitialData
             if (string.IsNullOrWhiteSpace(line)) continue;
 
             var columns = line.Split(';');
+
             var year = int.Parse(columns[0]);
             var title = columns[1];
             var studio = columns[2];
-            var producers = columns[3].Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var producers = SplitProducers(columns[3]);
             var isWinner = columns[4].Trim().Equals("yes", StringComparison.OrdinalIgnoreCase);
 
             foreach (var producer in producers)
@@ -35,5 +36,13 @@ public static class InitialData
         }
 
         context.SaveChanges();
+    }
+
+    private static List<string> SplitProducers(string producers)
+    {
+        var splitBy = new[] { ",", "and" };
+        return producers.Split(splitBy, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(p => p.Trim())
+                        .ToList();
     }
 }
